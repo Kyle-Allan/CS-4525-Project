60e467be292b88570dbcaab41a5e1d80ddf577a6
kylea <kyle.allan.597@gmail.com>
2024-11-30 10:32:20 -0400

    B+ implementation from geeksforgeeks

--- DatabaseProject/B+ Tree.py
+++ DatabaseProject/B+ Tree.py
@@ -1,101 +1,162 @@
 import math
 
+# Node creation
 class Node:
-    def __init__(self, order, is_leaf=False):
+    def __init__(self, order):
         self.order = order
-        self.is_leaf = is_leaf
+        self.values = []
         self.keys = []
-        self.children = []  # Pointers to child nodes or data values
-        self.next = None  # For linking leaf nodes
+        self.nextKey = None
+        self.parent = None
+        self.check_leaf = False
 
+    # Insert at the leaf
+    def insert_at_leaf(self, leaf, value, key):
+        if (self.values):
+            temp1 = self.values
+            for i in range(len(temp1)):
+                if (value == temp1[i]):
+                    self.keys[i].append(key)
+                    break
+                elif (value < temp1[i]):
+                    self.values = self.values[:i] + [value] + self.values[i:]
+                    self.keys = self.keys[:i] + [[key]] + self.keys[i:]
+                    break
+                elif (i + 1 == len(temp1)):
+                    self.values.append(value)
+                    self.keys.append([key])
+                    break
+        else:
+            self.values = [value]
+            self.keys = [[key]]
+
+
+# B plus tree
 class BplusTree:
     def __init__(self, order):
-        self.root = Node(order, is_leaf=True)
+        self.root = Node(order)
+        self.root.check_leaf = True
 
-    def insert(self, key, value):
-        node = self.root
-        if len(node.keys) == node.order - 1:
-            # Split root node
-            new_root = Node(node.order)
-            new_root.is_leaf = False
-            new_root.children.append(self.root)
-            self._split_child(new_root, 0)
-            self.root = new_root
-        self._insert_non_full(self.root, key, value)
+    # Insert operation
+    def insert(self, value, key):
+        value = str(value)
+        old_node = self.search(value)
+        old_node.insert_at_leaf(old_node, value, key)
 
-    def _insert_non_full(self, node, key, value):
-        if node.is_leaf:
-            # Insert into the correct position in the leaf
-            i = 0
-            while i < len(node.keys) and key > node.keys[i]:
-                i += 1
-            node.keys.insert(i, key)
-            node.children.insert(i, value)
+        if (len(old_node.values) == old_node.order):
+            node1 = Node(old_node.order)
+            node1.check_leaf = True
+            node1.parent = old_node.parent
+            mid = int(math.ceil(old_node.order / 2)) - 1
+            node1.values = old_node.values[mid + 1:]
+            node1.keys = old_node.keys[mid + 1:]
+            node1.nextKey = old_node.nextKey
+            old_node.values = old_node.values[:mid + 1]
+            old_node.keys = old_node.keys[:mid + 1]
+            old_node.nextKey = node1
+            self.insert_in_parent(old_node, node1.values[0], node1)
+
+    # Search operation for different operations
+    def search(self, value):
+        current_node = self.root
+        while(current_node.check_leaf == False):
+            temp2 = current_node.values
+            for i in range(len(temp2)):
+                if (value == temp2[i]):
+                    current_node = current_node.keys[i + 1]
+                    break
+                elif (value < temp2[i]):
+                    current_node = current_node.keys[i]
+                    break
+                elif (i + 1 == len(current_node.values)):
+                    current_node = current_node.keys[i + 1]
+                    break
+        return current_node
+
+    # Find the node
+    def find(self, value, key):
+        l = self.search(value)
+        for i, item in enumerate(l.values):
+            if item == value:
+                if key in l.keys[i]:
+                    return True
+                else:
+                    return False
+        return False
+
+    # Inserting at the parent
+    def insert_in_parent(self, n, value, ndash):
+        if (self.root == n):
+            rootNode = Node(n.order)
+            rootNode.values = [value]
+            rootNode.keys = [n, ndash]
+            self.root = rootNode
+            n.parent = rootNode
+            ndash.parent = rootNode
+            return
+
+        parentNode = n.parent
+        temp3 = parentNode.keys
+        for i in range(len(temp3)):
+            if (temp3[i] == n):
+                parentNode.values = parentNode.values[:i] + \
+                    [value] + parentNode.values[i:]
+                parentNode.keys = parentNode.keys[:i +
+                                                  1] + [ndash] + parentNode.keys[i + 1:]
+                if (len(parentNode.keys) > parentNode.order):
+                    parentdash = Node(parentNode.order)
+                    parentdash.parent = parentNode.parent
+                    mid = int(math.ceil(parentNode.order / 2)) - 1
+                    parentdash.values = parentNode.values[mid + 1:]
+                    parentdash.keys = parentNode.keys[mid + 1:]
+                    value_ = parentNode.values[mid]
+                    if (mid == 0):
+                        parentNode.values = parentNode.values[:mid + 1]
+                    else:
+                        parentNode.values = parentNode.values[:mid]
+                    parentNode.keys = parentNode.keys[:mid + 1]
+                    for j in parentNode.keys:
+                        j.parent = parentNode
+                    for j in parentdash.keys:
+                        j.parent = parentdash
+                    self.insert_in_parent(parentNode, value_, parentdash)
+
+# Print the tree
+def printTree(tree):
+    lst = [tree.root]
+    level = [0]
+    leaf = None
+    flag = 0
+    lev_leaf = 0
+
+    node1 = Node(str(level[0]) + str(tree.root.values))
+
+    while (len(lst) != 0):
+        x = lst.pop(0)
+        lev = level.pop(0)
+        if (x.check_leaf == False):
+            for i, item in enumerate(x.keys):
+                print(item.values)
         else:
-            # Find the correct child to recurse into
-            i = 0
-            while i < len(node.keys) and key > node.keys[i]:
-                i += 1
-            if len(node.children[i].keys) == node.order - 1:
-                self._split_child(node, i)
-                if key > node.keys[i]:
-                    i += 1
-            self._insert_non_full(node.children[i], key, value)
-
-    def _split_child(self, parent, index):
-        node = parent.children[index]
-        mid = len(node.keys) // 2
-        new_node = Node(node.order, is_leaf=node.is_leaf)
-        parent.keys.insert(index, node.keys[mid])
-        parent.children.insert(index + 1, new_node)
-        new_node.keys = node.keys[mid + 1:]
-        node.keys = node.keys[:mid]
-        if node.is_leaf:
-            new_node.children = node.children[mid:]
-            node.children = node.children[:mid]
-            new_node.next = node.next
-            node.next = new_node
-        else:
-            new_node.children = node.children[mid + 1:]
-            node.children = node.children[:mid + 1]
-
-    def search(self, key):
-        node = self.root
-        while not node.is_leaf:
-            i = 0
-            while i < len(node.keys) and key > node.keys[i]:
-                i += 1
-            node = node.children[i]
-        # Search within the leaf node
-        for i, item in enumerate(node.keys):
-            if key == item:
-                return node.children[i]
-        return None
-
-    def print_tree(self):
-        def print_level(nodes):
-            next_level = []
-            output = ""
-            for node in nodes:
-                output += str(node.keys) + " "
-                if not node.is_leaf:
-                    next_level.extend(node.children)
-            print(output)
-            if next_level:
-                print_level(next_level)
-        print_level([self.root])
+            for i, item in enumerate(x.keys):
+                print(item.values)
+            if (flag == 0):
+                lev_leaf = lev
+                leaf = x
+                flag = 1
 
 
+record_len = 3
+bplustree = BplusTree(record_len)
+bplustree.insert('5', '33')
+bplustree.insert('15', '21')
+bplustree.insert('25', '31')
+bplustree.insert('35', '41')
+bplustree.insert('45', '10')
 
-bplustree = BplusTree(order=4)
-bplustree.insert(1, "Value1")
-bplustree.insert(2, "Value2")
-bplustree.insert(3, "Value3")
-bplustree.insert(4, "Value4")
-bplustree.insert(5, "Value5")
-bplustree.insert(6, "Value6")
-bplustree.insert(7, "Value7")
+printTree(bplustree)
 
-bplustree.print_tree()
-result = bplustree.search(5)
-print(f"Found: {result}")
+if(bplustree.find('5', '34')):
+    print("Found")
+else:
+    print("Not found")
