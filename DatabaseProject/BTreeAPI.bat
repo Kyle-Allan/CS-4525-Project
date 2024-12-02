c9da825efb8f6b3b89189ea9e4a6d6626c9e81d3
kylea <kyle.allan.597@gmail.com>
2024-12-02 11:16:19 -0400

    bat file inserts 3 values and retrieves them by range query

--- DatabaseProject/BTreeAPI.bat
+++ DatabaseProject/BTreeAPI.bat
@@ -1,2 +1,4 @@
-curl -X POST http://127.0.0.1:5000/insert -H "Content-Type: application/json" -d "{\"time\": \"2024-12-02T12:10:00\", \"value\": 30}"
-
+curl -X POST http://127.0.0.1:5000/insert -H "Content-Type: application/json" -d "{\"time\": \"2024-12-02T12:00:00\", \"value\": 25}"
+curl -X POST http://127.0.0.1:5000/insert -H "Content-Type: application/json" -d "{\"time\": \"2024-12-02T12:05:00\", \"value\": 30}"
+curl -X POST http://127.0.0.1:5000/insert -H "Content-Type: application/json" -d "{\"time\": \"2024-12-02T12:10:00\", \"value\": 35}"
+curl "http://127.0.0.1:5000/query?start=2024-12-02T12:00:00&end=2024-12-02T12:15:00"
